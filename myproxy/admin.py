4e1e66e4b5c941e76d5cb9a7249102aebf676f81
zx576 <782744680@qq.com>
2017-05-25 09:27:00 +0800

    'add_ipaddr'

--- myproxy/admin.py
+++ myproxy/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Proxy
+from .models import Proxy, IpAddr
 
 @admin.register(Proxy)
 class ProxyAdmin(admin.ModelAdmin):
 
     date_hierarchy = 'created_time'
     list_filter = ('status','resourse','created_time')
+    list_per_page = 50
+
+
+@admin.register(IpAddr)
+class IpAddrAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_time'
     list_per_page = 50
