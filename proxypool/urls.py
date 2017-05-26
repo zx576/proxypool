7549f6318463dce5a2ef3c9a4af76c407a80af8a
zx576 <782744680@qq.com>
2017-05-26 18:48:24 +0800

    'update'

--- proxypool/urls.py
+++ proxypool/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^proxy/',include('myproxy.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^proxy/admin/', admin.site.urls),
 ]
