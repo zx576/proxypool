0b4e9b6d5b1f37f05767f7a4105351c9f581001d
zx576 <782744680@qq.com>
2017-06-30 12:09:32 +0800

    update

--- proxypool/settings.py
+++ proxypool/settings.py
@@ -119,6 +119,6 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/proxy/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
