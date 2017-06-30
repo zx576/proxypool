e8f5873a3cfefafe96d9ec7dfc03353b9ab91e59
Provinm <zhouxin@gmail.com>
2017-06-30 12:15:11 +0800

    uodate

--- myproxy/admin.py
+++ myproxy/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import Proxy, IpAddr
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
+
+
+class ProxyResource(resources.ModelResource):
+    class Meta:
+        model = Proxy
 
 @admin.register(Proxy)
-class ProxyAdmin(admin.ModelAdmin):
-
+class ProxyAdmin(ImportExportModelAdmin):
+    resource_class = ProxyResource
     date_hierarchy = 'created_time'
     list_filter = ('status','resourse','created_time')
     list_display = ('ip', 'port', 'head', 'type', 'status', 'district', 'Validated_time', 'failed_time', 'created_time', 'last_modified_time')
